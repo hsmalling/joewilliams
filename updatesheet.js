a63022633e200320f6d44cefa5fa0589b3d71aeb
hsmalling <hsmalling@hotmail.com>
2020-05-04 02:19:41 -0500

    removed javacript files

--- updatesheet.js
+++
@@ -1,9 +0,0 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwCQXG-RLwDfzZG23ZIDh_La7KUkwPoQdal6HaoSDkYDDdVzuA/exec'
-const form = document.forms['submit-to-google-sheet']
-
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => console.log('Success!', response))
-    .catch(error => console.error('Error!', error.message))
-})
